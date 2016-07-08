5dc7c58bdccf6f54c745b31085f9da80f5da365c
Christopher Nelson <chrisnelson@bssecure.net>
2016-07-08 19:50:45 +0100

    Update example_module.js

--- example_module.js
+++ example_module.js
@@ -6,7 +6,14 @@ function Example(module, bot) {
 	this.bot = bot;
 	this.module.SetName('Module Name');
 	this.module.RegisterCommand('command', ranks.User, function (data) { 
-		// that.bot.sendChat for example. Take a look at plugCubed/PlugAPI for more info
+		// that.bot is the bot commands from plugCubed/PlugAPI.
+		/* that.module = {
+			ModuleName: '', (string, the name of the module)
+			Logger: debug-logger, (debug log of the module)
+			Bot: plugapi, (is the same as the reference that is passed in to the module contructor)
+			RegisterCommand: function, (register a command, Params: commandName (string), userRank (int), callback (function)),
+			SetName: function, (sets the modules name, Params: moduleName (string))
+		}*/
 	});
 };
 
